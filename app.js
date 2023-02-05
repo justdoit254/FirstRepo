fb4f7cfbdcf11e720d6e32a28d836b54999220cf
Shipul <shipul2910@gmail.com>
2023-02-05 18:12:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// npm - global command, comes with node
+// npm --version
+
+// local dependency - to use the package for any particular project
+// npm i <packageName>
+
+// global dependency - to use the package in any project
+// npm install -g <packageName>
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties, etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
